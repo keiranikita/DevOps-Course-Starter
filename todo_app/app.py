67e3164935e071367dd8bc8429603a8e8a87ffa0
Keira Marshall <k.marshall@uk.zurich.com>
2021-02-19 14:47:37 +0000

    Commiting Changes for project Submission

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,15 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items, add_item, save_item, get_item
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
 @app.route('/')
 def index():
-    return 'Hello World!'
+    todo_items=get_items()
+    return render_template('index.html', todo_items=todo_items)
 
+@app.route('/', methods=['POST'])
+def add_todoitem():
+    title=request.form.get("additem")
+    add_item(title)
+    return redirect('/')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
